356a17ffa72d7c855392aa0a1c3106d3e6563098
Sudhanshuo8 <58129312+Sudhanshuo8@users.noreply.github.com>
2020-10-04 23:33:23 +0530

    Add files via upload

---
+++ Assignment_Day4/36/Math1.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class MathOpe {
+  
+
+  int multiply(int a, int b) {
+        return a * b;
+  }
+
+  float multiply(float a,float b, float c){
+    return (float)(a*b*c);
+  }
+
+  int multiply(int arr[]){
+    int mul=1;
+    for(int i=0;i<arr.length;i++){
+        mul=mul*arr[i];
+    }
+    return mul;
+  }
+  
+  double multiply(double a, int b, int c){
+    return a*b*c;
+  }
+  
+}
+
+class Math1 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    MathOpe m1 = new MathOpe();
+    
+    System.out.println(
+      "Multiplication of two integer numbers is : " + m1.multiply(2,3)
+    );
+
+    System.out.println(
+      "Multiplication of three float numbers is : " + m1.multiply(2.5f,2.5f,2.5f)
+    );
+
+    int arr[]={2,2,2};
+
+    System.out.println(
+      "Multiplication of array elements is : " + m1.multiply(arr)
+    );
+
+    System.out.println(
+      "Multiplication of one double and two integer numbers is : " + m1.multiply(2.5,2,2)
+    );
+
+  }
+}
