ab4ece190af6719ce917160cb4fa3ba5da1ee68a
Sudhanshuo8 <58129312+Sudhanshuo8@users.noreply.github.com>
2020-10-06 21:48:33 +0530

    Add files via upload

---
+++ Assignment_Day7/59/Voter1.java
@@ -0,0 +1,35 @@
+
+class Voter1{
+    int voterId;
+    String name;
+    int age;
+    public Voter(int voterId, String name, int age) {
+        this.voterId = voterId;
+        this.name = name;
+        
+        if(age <= 18)
+        {
+            try {
+                throw new Exception("invalid age for voter");
+            }
+            catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        else
+        {
+            this.age = age;
+            System.out.println("voterId = "+voterId+" name = "+name+" age = "+age);
+        }
+    }
+    
+}
+public class Voter1 {
+
+
+public static void main(String[] args) {
+    Voter v = new Voter(1,"Mohini",17);	
+
+}
+
+}
